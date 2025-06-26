4edfd7f02a0c0e18566c787b6a41cadc449b93dc
Jimmyliang46 <108730638+Jimmyliang46@users.noreply.github.com>
2025-06-26 13:44:03 -0400

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,14 @@
+# Create my own variables and print them 
+# 1. Store my name 
+# 2. Store my age
+# 3. Store my favorite color
+# 4. Store my favorite food
+# 5. Store my favorite hobby 
+
+name = "Jimmy"
+age = 30
+favorite_color = "Green"
+favorite_food = "Pasta"
+Favorite_hobby = "eat"
+
+print("My name is", name + ". I am", age, "years old. My favorite color is", favorite_color + ".", "I love to", Favorite_hobby + " and my favorite food is", favorite_food) 
